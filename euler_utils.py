aaffa73aa0c1e8aed524f075e8516b85d6cce3b8
Silviu Apostu <git@silviuapostu.com>
2016-11-23 15:47:20 +0100

    move common methods in utility file

---
+++ euler_utils.py
@@ -0,0 +1,34 @@
+'''
+A collection of utility methods for common operations in Project Euler problems
+'''
+
+from math import sqrt, floor
+
+
+def is_prime(x):
+    if x < 2:
+        return False
+    i = 2
+    while i < int(floor(sqrt(x)))+1:
+        if x % i == 0:
+            return False
+        i += 1
+    return True
+
+
+def erathos(n):
+    '''
+    Create an Erathostenes sieve for prime numbers up to n
+    '''
+    l = list(range(n))
+    l[1] = 0
+    sqrt = int(round(n**0.5))
+    for i in range(2, sqrt + 1):
+        if l[i]:
+            l[i * i: n: i] = [0] * len(range(i * i, n, i))
+    return list(filter(None, l))
+
+
+def is_pandigital(x):
+    digits = list(str(x))
+    return ''.join(sorted(digits)) == '123456789'
